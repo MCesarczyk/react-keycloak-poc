07a793c765c42e757b11dfcc3e272252703dfab4
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-07-10 19:42:51 +0200

    feat: restore scrict mode inside of keycloak provider, remove obsolete files

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { Nav } from './Nav';
 import { Home } from './Homepage';
 import { Secured } from './Secured';
-import { keycloak } from './keycloak';
 import { PrivateRoute } from './PrivateRoute';
 
 console.log(process.env);
 
 export const App = () => (
   <div>
-    <ReactKeycloakProvider authClient={keycloak}>
-      <Nav />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/secured"
-            element={
-              <PrivateRoute>
-                <Secured />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </ReactKeycloakProvider>
+    <Nav />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/secured"
+          element={
+            <PrivateRoute>
+              <Secured />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { keycloak } from './keycloak';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+  <ReactKeycloakProvider authClient={keycloak}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ReactKeycloakProvider>
 );
 
 reportWebVitals();
