477c0a05e1907d25a113f8f3cbe23bdc1dbc0347
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-07-10 19:28:31 +0200

    feat: create presentation view, update style

--- src/Secured.tsx
+++ src/Secured.tsx
@@ -1,7 +1,34 @@
+import { useKeycloak } from '@react-keycloak/web';
+import { aois } from './data';
+
 export const Secured = () => {
+  const { keycloak } = useKeycloak();
+
+  const isAdmin = keycloak.hasResourceRole('admin');
+  const isUser = keycloak.hasResourceRole('user');
+  const canViewAreas = keycloak.hasResourceRole('view-aois');
+  const canViewWaterQuality = keycloak.hasResourceRole('view-water-quality');
+  const canViewWaterAreas = keycloak.hasResourceRole('view-water-surface');
+
   return (
     <div>
-      <h1 className="text-black text-4xl">Welcome to the Protected Page.</h1>
+      <h1 className="text-black text-4xl">Protected Page.</h1>
+      <p>
+        Current user {isAdmin ? 'HAS' : 'DOES NOT HAVE'} access to the ADMIN
+        role.
+      </p>
+      <p>
+        Current user {isUser ? 'HAS' : 'DOES NOT HAVE'} access to the USER role.
+      </p>
+      {canViewAreas && (
+        <ul>
+          {aois.map((aoi) => (
+            <li key={aoi.id}>{aoi.name}</li>
+          ))}
+        </ul>
+      )}
+      <p>{canViewWaterQuality && <a href="/water-quality">Water Quality</a>}</p>
+      <p>{canViewWaterAreas && <a href="/water-areas">Water Areas</a>}</p>
     </div>
   );
 };
---
+++ src/data.ts
@@ -0,0 +1,42 @@
+export const aois = [
+  {
+      "id": 224,
+      "name": "GS Rembelszczyzna Mory 0",
+      "display_name": "Rembelszczyzna-Mory",
+      "description": "",
+      "created_date": "2023-02-03T12:38:59.227172Z",
+      "active": true
+  },
+  {
+      "id": 244,
+      "name": "GS Słomianki Kraków Big",
+      "display_name": "Słomniki-Kraków",
+      "description": "",
+      "created_date": "2023-02-23T10:07:00.155810Z",
+      "active": true
+  },
+  {
+      "id": 235,
+      "name": "GS Polska Słowacja Big",
+      "display_name": "Polska-Słowacja",
+      "description": "",
+      "created_date": "2023-02-21T10:54:27.344059Z",
+      "active": true
+  },
+  {
+      "id": 236,
+      "name": "GS Mory Wola Karczewska Big",
+      "display_name": "Mory-Wola Karczewska",
+      "description": "",
+      "created_date": "2023-02-21T10:54:29.363186Z",
+      "active": true
+  },
+  {
+      "id": 237,
+      "name": "GS Skoczów Oświęcim Big",
+      "display_name": "Skoczów-Oświęcim",
+      "description": "",
+      "created_date": "2023-02-21T10:54:30.910429Z",
+      "active": true
+  }
+];
